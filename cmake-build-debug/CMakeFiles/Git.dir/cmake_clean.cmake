8add3c6997c481112efa097d398d45c7c4c2a2ba
Mateusz Lach <235910@edu.p.lodz.pl>
2021-02-23 10:23:41 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Git.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Git.dir/main.cpp.obj"
+  "Git.exe"
+  "Git.exe.manifest"
+  "Git.lib"
+  "Git.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Git.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Git.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Git.
+# This may be replaced when dependencies are built.
